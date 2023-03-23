e6e40d96eeb4c67aff65705432e9ea1199a1221e
Sambhav Mehta <sambhavmehta361@gmail.com>
2023-03-23 11:09:33 +0530

    Day 7 done

---
+++ src/Day7_2455.java
@@ -0,0 +1,20 @@
+public class Day7_2455 {
+    public static void main(String[] args){
+        int [] arr=new int[]{1,6,5};
+        System.out.println(averageValue(arr));
+    }
+    static int averageValue(int[] nums) {
+        int count=0;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%6==0){
+                sum=sum+nums[i];
+                count++;
+            }
+        }
+        if(count==0){
+            return 0;
+        }
+        return sum/count;
+    }
+}
