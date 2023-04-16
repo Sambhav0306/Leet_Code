73eeb894bdd6ac2fe48baf44b39bb188bf9113f6
Sambhav Mehta <sambhavmehta361@gmail.com>
2023-04-16 17:57:39 +0530

    Day 31 done

---
+++ src/Day31_1429.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Day31_1429 {
+    static int kthFactor(int n, int k) {
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                list.add(i);
+            }
+        }
+        if(k>list.size()){
+            return -1;
+        }
+        return list.get(k-1);
+    }
+}
