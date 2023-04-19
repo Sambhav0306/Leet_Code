d937729d43a5b897a3a6078fd2c34e21c2f44236
Sambhav Mehta <sambhavmehta361@gmail.com>
2023-04-19 14:28:42 +0530

    Day 34 done

---
+++ src/Day34_921.java
@@ -0,0 +1,13 @@
+public class Day34_921 {
+    int minAddToMakeValid(String s) {
+        Stack<Character> stack= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(!stack.isEmpty() && s.charAt(i)==')'&&stack.peek()=='('){
+                stack.pop();
+            } else{
+                stack.push(s.charAt(i));
+            }
+        }
+        return stack.size();
+    }
+}
