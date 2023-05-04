ff7257ab299494d45545a9f9c18f3e3d3320c0c7
Sambhav Mehta <sambhavmehta361@gmail.com>
2023-05-05 00:38:52 +0530

    Day 52 done

---
+++ src/Day52_29.java
@@ -0,0 +1,15 @@
+public class Day52_29 {
+    int divide(int dividend, int divisor) {
+        int ans=dividend/divisor;
+        if( dividend==-2147483648 && divisor==-1){
+            return 2147483647;
+        }
+        if(ans>Math.pow(2,31)-1){
+            return (int)Math.pow(2,31)-1;
+        }
+        if(ans<Math.pow(-2,31)-1){
+            return (int)Math.pow(-2,31);
+        }
+        return ans;
+    }
+}
