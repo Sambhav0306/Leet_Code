a1b154b8a6e307725fd8b12b01314fec91090ad7
Sambhav Mehta <sambhavmehta361@gmail.com>
2023-05-05 00:53:33 +0530

    Day 52 done

---
+++ src/Day52_27.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Day52_27 {
+    int removeElement(int[] arr, int k) {
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==k){
+                count++;
+                arr[i]=-1;
+            }
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length/2;i++){
+            if(arr[arr.length-1-i]==-1){
+                break;
+            }
+            int temp=arr[i];
+            arr[i]=arr[arr.length -1-i];
+            arr[arr.length -1-i]=temp;
+
+        }
+        return arr.length-count;
+    }
+}
