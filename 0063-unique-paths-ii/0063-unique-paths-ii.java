e3781ac9d78e5295ef0686fd0164a83c0734c874
Sambhav0306 <122718385+Sambhav0306@users.noreply.github.com>
2023-07-10 11:14:55 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (30.30%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,27 +2,45 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
         int m=arr.length;
         int n=arr[0].length;
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
         if(arr[0][0]==1){
             return 0;
         }
-        return f(arr,m-1,n-1,dp);
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 &&j==0){
+                    dp[0][0]=1;
+                    continue;
+                }
+                if(arr[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                int up=0;
+                int right=0;
+                if(i>0){
+                    up=dp[i-1][j];
+                }
+                if(j>0){
+                    right=dp[i][j-1];
+                }
+                dp[i][j]=up+right;
+            }
+        }
+        return dp[m-1][n-1];
     }
-    public int f(int[][] arr,int i,int j,int[][] dp){
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(i<0 || j<0 || arr[i][j]==1){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int up=f(arr,i-1,j,dp);
-        int right=f(arr,i,j-1,dp);
-        return dp[i][j]=up+right;
-    }
+    // public int f(int[][] arr,int i,int j,int[][] dp){
+    //     if(i==0 && j==0){
+    //         return 1;
+    //     }
+    //     if(i<0 || j<0 || arr[i][j]==1){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int up=f(arr,i-1,j,dp);
+    //     int right=f(arr,i,j-1,dp);
+    //     return dp[i][j]=up+right;
+    // }
 }
