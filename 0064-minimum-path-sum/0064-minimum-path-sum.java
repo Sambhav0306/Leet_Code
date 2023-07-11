2b578375a70b56d5da12f22629227c3a1887381c
Sambhav0306 <122718385+Sambhav0306@users.noreply.github.com>
2023-07-11 12:27:23 +0530

    Time: 3 ms (30.75%), Space: 43.9 MB (71.12%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -6,20 +6,42 @@ class Solution {
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return f(grid,n-1,m-1,dp);
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[0][0]=grid[0][0];
+                    continue;
+                }
+                int up=grid[i][j];
+                int left=grid[i][j];
+                if(i>0){
+                    up+=dp[i-1][j];
+                }else{
+                    up+=(int)Math.pow(10,9);
+                }
+                if(j>0){
+                    left+=dp[i][j-1];
+                }else{
+                    left+=(int)Math.pow(10,9);
+                }
+                dp[i][j]=Math.min(up,left);
+            }
+        }
+        return dp[n-1][m-1];
+        // return f(grid,n-1,m-1,dp);
     }
-    public int f(int[][] grid,int i, int j,int[][] dp){
-        if(i==0 && j==0){
-            return grid[0][0];
-        }
-        if(i<0 || j<0){
-            return (int)Math.pow(10,9);
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int up=grid[i][j]+f(grid,i-1,j,dp);
-        int left=grid[i][j]+f(grid,i,j-1,dp);
-        return dp[i][j]=Math.min(up,left);
-    }
+    // public int f(int[][] grid,int i, int j,int[][] dp){
+    //     if(i==0 && j==0){
+    //         return grid[0][0];
+    //     }
+    //     if(i<0 || j<0){
+    //         return (int)Math.pow(10,9);
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int up=grid[i][j]+f(grid,i-1,j,dp);
+    //     int left=grid[i][j]+f(grid,i,j-1,dp);
+    //     return dp[i][j]=Math.min(up,left);
+    // }
 }
