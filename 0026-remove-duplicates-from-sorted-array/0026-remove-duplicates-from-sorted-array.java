e40e64dec9c14bab974bf050f1de001fd713539e
Sambhav0306 <122718385+Sambhav0306@users.noreply.github.com>
2023-07-27 19:59:44 +0530

    Time: 1 ms (99.16%), Space: 43.8 MB (85.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,24 +1,32 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+       // // Arrays.sort(nums);
+       // for(int i=0;i<nums.length-1;i++){
+       //     // for(int j=i+1;j<nums.length;j++){
+       //         // if(nums[i]==101|| nums[i]!=nums[j]){
+       //         //     break;
+       //         // }
+       //         if(nums[i]==nums[i+1]){
+       //             nums[i]=101;
+       //         }
+       //     }
+       // // }  
        // Arrays.sort(nums);
-       for(int i=0;i<nums.length-1;i++){
-           // for(int j=i+1;j<nums.length;j++){
-               // if(nums[i]==101|| nums[i]!=nums[j]){
-               //     break;
-               // }
-               if(nums[i]==nums[i+1]){
-                   nums[i]=101;
-               }
-           }
-       // }  
-       Arrays.sort(nums);
-       int count=0;
-       for(int i=0;i<nums.length;i++){
-           if(nums[i]==101){
-               break;
-           }
-           count++;
-       }
-       return count++;
+       // int count=0;
+       // for(int i=0;i<nums.length;i++){
+       //     if(nums[i]==101){
+       //         break;
+       //     }
+       //     count++;
+       // }
+       // return count++;
+        int i=1;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[j-1]){
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
