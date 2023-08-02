974a79f269b47962d47126799dd942332b185342
Sambhav0306 <122718385+Sambhav0306@users.noreply.github.com>
2023-08-02 13:05:19 +0530

    Time: 9 ms (69.18%), Space: 43.9 MB (24.32%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,28 +1,31 @@
-class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        // int count=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]==0){
-        //         count++;
-        //     }
-        // }
-        // if(count>0){
-        //     return f(nums,target,nums.length-1)+(int)Math.pow(2,count-1);
-        // }
-        return f(nums,target,nums.length-1);
-    }
-    public int f(int[] nums,int target,int ind){
-        if(ind==0){
-            if(nums[0]==0 && target==0){
-                return 2;
-            }
-            if(nums[0]==target || nums[0]==-target){
-               return 1;
-            }
-            return 0;
+public class Solution {
+    int total;
+    
+    public int findTargetSumWays(int[] nums, int S) {
+        total = Arrays.stream(nums).sum();
+        
+        int[][] memo = new int[nums.length][2 * total + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
         }
-        int pos=f(nums,target-nums[ind],ind-1);
-        int neg=f(nums,target+nums[ind],ind-1);
-        return pos+neg;
+        return calculate(nums, 0, 0, S, memo);
+    }
+    
+    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (memo[i][sum + total] != Integer.MIN_VALUE) {
+                return memo[i][sum + total];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + total] = add + subtract;
+            return memo[i][sum + total];
+        }
     }
 }
