8ebc863026ab7bab3577e8ef316fff99dd84e115
Sambhav0306 <122718385+Sambhav0306@users.noreply.github.com>
2023-09-03 15:31:00 +0530

    Time: 13 ms (11.35%), Space: 43.9 MB (81.69%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.java
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.java
@@ -1,4 +1,5 @@
 class Solution {
+    List<Integer> list=new ArrayList<>();
     public boolean hasGroupsSizeX(int[] deck) {
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<deck.length;i++){
@@ -10,13 +11,21 @@ class Solution {
         }
         int min=Collections.min(map.values());
         if(min==1) return false;
-        for(int i=2;i<=min;i++){
+        f(min);
+        for(int i=0;i<list.size();i++){
             int count=0;
             for(int j:map.keySet()){
-            if(map.get(j)%i!=0) count++;
+            if(map.get(j)%list.get(i)!=0) count++;
         }
         if(count==0) return true;
         }
         return false;
+    }
+    public void f(int min){
+        int i=min;
+        while(i!=1){
+            if(min%1==0) list.add(i);
+            i--;
+        }
     }
 }
