70fdc6a58f5578fe4dd2550ea487d961e3dbadbb
Sambhav0306 <122718385+Sambhav0306@users.noreply.github.com>
2023-09-04 16:22:08 +0530

    Time: 11 ms (100.00%), Space: 41.4 MB (76.45%) - LeetHub

---
+++ 0677-map-sum-pairs/0677-map-sum-pairs.java
@@ -0,0 +1,27 @@
+class MapSum {
+    HashMap<String,Integer> map;
+    public MapSum() {
+        map=new HashMap<>();
+    }
+    
+    public void insert(String key, int val) {
+        map.put(key,val);
+    }
+    
+    public int sum(String prefix) {
+        int sum=0;
+        for(String s:map.keySet()){
+            if(s.indexOf(prefix)==0){
+                sum+=map.get(s);
+            }
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
