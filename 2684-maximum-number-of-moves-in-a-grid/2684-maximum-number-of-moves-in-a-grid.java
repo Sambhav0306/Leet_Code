974c0c688759430687ea26637cc8ddbb83f5c55d
Sambhav0306 <122718385+Sambhav0306@users.noreply.github.com>
2023-09-10 22:25:07 +0530

    Time: 215 ms (6.32%), Space: 54 MB (92.10%) - LeetHub

---
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxMoves(int[][] grid) {
+        int max=0;
+        for(int i=0;i<grid.length;i++){
+            int[][] dp=new int[grid.length][grid[0].length];
+            for(int j=0;j<grid.length;j++){
+                Arrays.fill(dp[j],-1);
+            }
+            max=Math.max(max,f(i,0,grid,dp));
+        }
+        return max;
+    } 
+    public int f(int i,int j,int[][] nums,int[][] dp){
+        if(j==nums[0].length-1){
+            return 0;
+        }
+        int up=0;
+        int str=0;
+        int down=0;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(i-1>=0 && nums[i][j]<nums[i-1][j+1]){
+            up=1+f(i-1,j+1,nums,dp);
+        }
+        if(nums[i][j]<nums[i][j+1]){
+            str=1+f(i,j+1,nums,dp);
+        }
+        if(i+1<nums.length && nums[i][j]<nums[i+1][j+1]){
+            down=1+f(i+1,j+1,nums,dp);
+        }
+        return dp[i][j]=Math.max(up,Math.max(str,down));
+    }
+}
