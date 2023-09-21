c30fc6dbf129762aac0229ff523e99a98cc14a8a
Sambhav0306 <122718385+Sambhav0306@users.noreply.github.com>
2023-09-21 16:04:26 +0530

    Time: 50 ms (98.99%), Space: 41.6 MB (81.99%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,22 +1,30 @@
 class Solution {
-    public int beautySum(String s) {
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            Map<Character,Integer> map=new HashMap<>();
-            for(int j=i;j<s.length();j++){
-                char c=s.charAt(j);
-                if(map.containsKey(c)){
-                  map.put(c,map.get(c)+1);
-                }else{
-                  map.put(c,1);
-                }
-                int max=Collections.max(map.values());
-                int min=Collections.min(map.values());
-                if(max-min!=0){
-                    count+=max-min;
-                }
+    public int beautySum(String str) {
+        char[]arr=str.toCharArray();
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            int[] freq = new int[26];
+
+            for (int j = i; j < arr.length; j++)
+            {
+                freq[arr[j] - 'a']++;
+
+                int maxFreq = Integer.MIN_VALUE;
+                int minFreq = Integer.MAX_VALUE;
+
+                for (int k = 0; k < 26; k++) 
+                  if(freq[k]!=0) 
+                  {
+                    maxFreq=Math.max(maxFreq,freq[k]);
+                    minFreq=Math.min(minFreq,freq[k]);
+                  }
+
+                sum += maxFreq - minFreq;
             }
         }
-        return count;
+
+        return sum;
     }
 }
