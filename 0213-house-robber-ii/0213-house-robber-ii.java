e96e1be5cb06a41b8301bf344e2fb12a05c593bb
Sambhav Mehta <122718385+Sambhav0306@users.noreply.github.com>
2024-02-13 02:09:01 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (13.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,25 @@
 class Solution {
    public int rob(int[] nums) {
-      if (nums.length == 1) return nums[0];  
-      return Math.max(rob0(nums), rob1(nums));
+       int[] dp1=new int[nums.length];
+       int[] dp2=new int[nums.length];
+      Arrays.fill(dp1,-1);
+      Arrays.fill(dp2,-1);
+      int a1= nums[0]+f1(nums,2,dp1);
+      int a2=f2(nums,1,dp2); 
+      return Math.max(a1,a2);
     }
-    
-    public int rob0(int[] nums){
-      int preMax = 0, curMax = 0;
-      for(int i = 0; i < nums.length - 1; i++){
-        int t = curMax;
-        curMax = Math.max(preMax + nums[i], curMax);
-        preMax = t;  
-      }  
-      return curMax;
+    public int f1(int[] nums,int i,int[] dp){
+        if(i>=nums.length-1) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int not_take=f1(nums,i+1,dp);
+        int take=nums[i]+f1(nums,i+2,dp);
+        return dp[i]=Math.max(take,not_take);
     }
-    
-    public int rob1(int[] nums){
-      int preMax = 0, curMax = 0;
-      for(int i = 1; i < nums.length; i++){
-        int t = curMax;
-        curMax = Math.max(preMax + nums[i], curMax);
-        preMax = t;  
-      }  
-      return curMax;  
+    public int f2(int[] nums,int i,int[] dp){
+        if(i>=nums.length) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int not_take=f2(nums,i+1,dp);
+        int take=nums[i]+f2(nums,i+2,dp);
+        return dp[i]=Math.max(take,not_take);
     }
 }
