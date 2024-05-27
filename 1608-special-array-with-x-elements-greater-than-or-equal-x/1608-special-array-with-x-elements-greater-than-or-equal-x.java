ab4a231f2d34d13451dc6e76214a42ca1d648440
Sambhav Mehta <122718385+Sambhav0306@users.noreply.github.com>
2024-05-27 09:47:02 +0530

    Time: 2 ms (47.70%), Space: 40.6 MB (99.19%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,11 +1,35 @@
 class Solution {
+    private int getFirstGreaterOrEqual(int[] nums, int val) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        int index = nums.length;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] >= val) {
+                index = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return index;
+    }
+
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
-        int n=nums.length;
-        for(int i=1;i<n;i++){
-            if(i<=nums[n-i] && i-1>=0 && i>nums[n-i-1] ) return i;
+
+        int N = nums.length;
+        for (int i = 1; i <= N; i++) {
+            int k = getFirstGreaterOrEqual(nums, i);
+
+            if (N - k == i) {
+                return i;
+            }
         }
-        if(nums[0]>=n) return n;
+
         return -1;
     }
 }
